65f13b5571348e7e2aa413093a80a4f969d449f1
AndyWilliam96 <153957322+andybrocklesby@users.noreply.github.com>
2024-03-20 19:50:03 -0500

    Initial Push

---
+++ app.js
@@ -0,0 +1,60 @@
+import services from './data.js'
+
+const serviceContainer = document.getElementById('service-container')
+const footer = document.getElementById('footer')
+const contactForm = document.getElementById('form')
+const contactSection = document.getElementById('contact')
+
+contactForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    const form = document.getElementById('form')
+    const formData = new FormData(form)
+    const formName = formData.get('name');
+
+    contactSection.innerHTML = `Thanks ${formName}, we will get back to you shortly...`
+})
+
+
+
+const generateService = () => {
+    let serviceHtml = ''
+    
+    services.forEach(service => {
+
+        
+
+        const {
+            title,
+            image,
+            description,
+            price
+        } = service;
+
+
+        serviceHtml += `
+                <div class="service">
+                    <img class="service-img" src="${image}" alt="">
+                    <div class="service-title">${title}</div>
+                    <div class="service-description">${description}</div>
+                    <div class="service-price">Starting at ${price}</div>
+                </div>
+        `
+        
+    })
+    return serviceHtml
+}
+
+const createFooterCopyright = () => {
+    const year = new Date()
+
+    return `&copy ${year.getFullYear()} NorthWest Finesse | All Rights Reserved`
+}
+
+
+const render = () => {
+    serviceContainer.innerHTML = generateService()
+    footer.innerHTML = createFooterCopyright()
+}
+
+render()
---
+++ data.js
@@ -0,0 +1,22 @@
+const services = [
+    {
+        title: "Maintenance Wash",
+        image: './images/service1.webp',
+        description: "Experience our meticulous 2 bucket method automotive maintenance wash for a scratch-free, glossy finish. Our trained technicians use high-quality shampoo and premium microfiber wash mitts, followed by gentle drying with soft towels. Optional services like spray wax or sealant application provide added protection and shine. Trust us to keep your vehicle looking its best.",
+        price: "$75"
+    },
+    {
+        title: "Decontamination Wash",
+        image: './images/service2.webp',
+        description: "Indulge in our thorough decontamination wash featuring a clay bar treatment and polish for a renewed, showroom-worthy finish. Our skilled team meticulously removes embedded contaminants using a premium clay bar, followed by a polishing step to eliminate imperfections and enhance shine. This process revitalizes your vehicle's appearance, leaving it smooth, glossy, and protected. Trust us to bring back the brilliance of your vehicle's paintwork with our professional-grade decontamination wash.",
+        price: "$145"
+    },
+    {
+        title: "Enhancement Wash",
+        image: './images/service3.webp',
+        description: "Experience the ultimate enhancement wash, encompassing our meticulous decontamination wash with a clay bar treatment and polish, as well as an added refining polish and sealant for maximum protection and shine. Our skilled technicians remove embedded contaminants, polish away imperfections, and apply a premium sealant to leave your vehicle looking flawless, glossy, and shielded from environmental elements. Trust us to elevate your vehicle's appearance with our comprehensive enhancement wash, delivering a showroom-worthy finish that lasts.",
+        price: "$265"
+    }
+]
+
+export default services
