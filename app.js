59ea5e40b1c05118cb8d3c56dfef04e9e2db2df9
AndyWilliam96 <153957322+andybrocklesby@users.noreply.github.com>
2024-03-20 20:04:46 -0500

    Update .js

--- app.js
+++ app.js
@@ -7,31 +7,22 @@ const contactSection = document.getElementById('contact')
 
 contactForm.addEventListener('submit', function(e){
     e.preventDefault()
-    
     const form = document.getElementById('form')
     const formData = new FormData(form)
     const formName = formData.get('name');
-
     contactSection.innerHTML = `Thanks ${formName}, we will get back to you shortly...`
 })
 
 
-
 const generateService = () => {
     let serviceHtml = ''
-    
     services.forEach(service => {
-
-        
-
         const {
             title,
             image,
             description,
             price
         } = service;
-
-
         serviceHtml += `
                 <div class="service">
                     <img class="service-img" src="${image}" alt="">
@@ -39,15 +30,14 @@ const generateService = () => {
                     <div class="service-description">${description}</div>
                     <div class="service-price">Starting at ${price}</div>
                 </div>
-        `
-        
+        ` 
     })
     return serviceHtml
 }
 
+
 const createFooterCopyright = () => {
     const year = new Date()
-
     return `&copy ${year.getFullYear()} NorthWest Finesse | All Rights Reserved`
 }
 
@@ -56,5 +46,6 @@ const render = () => {
     serviceContainer.innerHTML = generateService()
     footer.innerHTML = createFooterCopyright()
 }
+
 
 render()
